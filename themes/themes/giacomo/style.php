71d6813a8431f83132f80cc85993589878a99e2e
flocha <flocha2000-agile@yahoo.fr>
2012-10-24 11:12:49 +0200

    recup de fichiers FTP manquant et début de sprite pour '.post blockquote'

--- themes/themes/giacomo/style.php
+++ themes/themes/giacomo/style.php
@@ -375,9 +375,19 @@ que le prelude soit visible */
 
 .post blockquote {
 	font-family: Tahoma, "Lucida Grande", "Trebuchet MS", sans-serif;
-	background: #f5f5f5 url(images/quote.gif) no-repeat 4px 4px;
 	margin: 2em;
 	padding: 3px 2em;
+
+	/* ALBATOR ancienne version sans sprite */
+	background: #f5f5f5 url(images/quote.gif) no-repeat 4px 4px; 
+/*
+	background-color: #f5f5f5;
+	background-image: url(images/sprite.png);
+	background-repeat: no-repeat;
+	background-attachment: 4px;
+	background-position: -0px -0px;
+	background-width:0px;
+*/
 }
 
 .post blockquote p {
