7705d3ec7d0cad27fe5bac58d761b8b9bccebe92
fchabanois <flocha2000-agile@yahoo.fr>
2012-10-30 17:03:06 +0100

    refactoring

--- themes/giacomo/cacheImage.php
+++ themes/giacomo/cacheImage.php
@@ -1,6 +1,6 @@
 <?php
 header('Expires: '.gmdate('D, d M Y H:i:s \G\M\T', time() + 864000));
 
-header("Content-type: image/".pathinfo($_GET['imageName'],PATHINFO_EXTENSION));
-readfile($_GET['imageName']);
+header("Content-type: image/".pathinfo($_GET['file'],PATHINFO_EXTENSION));
+readfile($_GET['file']);
 ?>
