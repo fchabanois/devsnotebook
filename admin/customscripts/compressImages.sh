a2dd4a2982880d944eb3295a0e3534e6acb6904c
flocha <flocha2000-agile@yahoo.fr>
2012-10-31 13:32:17 +0100

    compression des PNG de public/billets et themes/giacomo des JPG également en écrasant les fichiers

--- admin/customscripts/compressImages.sh
+++ admin/customscripts/compressImages.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-#Compress les image png dans un repertoire gz
-rm -rf ../../themes/giacomo/images/gz/*
-lib/pngcrush -d ../../themes/giacomo/images/gz ../../themes/giacomo/images/*png
+# Compression des images JPG et PNG d'un repertoire donné
+#	Prend en compte les fichiers cachés et les différences de casse dans l'extension (coucou.JPG ou coucou.jpg)
 
-rm -rf ../../public/Billets/gz/* 
-lib/pngcrush -d ../../public/Billets/gz ../../public/Billets/*png
+IMAGES_DIR="../../themes/giacomo/images ../../public/Billets"
+
+for imgDir in $IMAGES_DIR
+do
+	#compress PNG
+	for image in `ls -a $imgDir/* | grep -i png`
+	do
+		lib/pngcrush -ow $imgDir/$image
+	done
+
+	#compress JPG
+	for image in `ls -a $imgDir/* | grep -i jpg`
+	do
+		lib/jpegoptim --strip-all $image
+	done
+done
