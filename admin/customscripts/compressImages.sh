963fb53cf435c6f3999155ad48714f7589d17f34
flocha <flocha2000-agile@yahoo.fr>
2012-10-31 15:41:00 +0100

    compression images

--- admin/customscripts/compressImages.sh
+++ admin/customscripts/compressImages.sh
@@ -3,19 +3,21 @@
 # Compression des images JPG et PNG d'un repertoire donné
 #	Prend en compte les fichiers cachés et les différences de casse dans l'extension (coucou.JPG ou coucou.jpg)
 
+set -x
+
 IMAGES_DIR="../../themes/giacomo/images ../../public/Billets"
 
 for imgDir in $IMAGES_DIR
 do
 	#compress PNG
-	for image in `ls -a $imgDir/* | grep -i png`
+	for image in `ls -a $imgDir/ | grep -i png`
 	do
 		lib/pngcrush -ow $imgDir/$image
 	done
 
 	#compress JPG
-	for image in `ls -a $imgDir/* | grep -i jpg`
+ 	for image in `ls -a $imgDir/ | grep -i jpg`
 	do
-		lib/jpegoptim --strip-all $image
+		lib/jpegoptim --strip-all $imgDir/$image
 	done
 done
